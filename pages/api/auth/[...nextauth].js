2d600297ed83251222739ca08534126af3bb40f6
Glenn Pickett <donnegas@gmail.com>
2022-01-12 13:50:17 -0500

    NextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,5 +8,20 @@ export default NextAuth({
           clientId: process.env.FACEBOOK_ID,
           clientSecret: process.env.FACEBOOK_SECRET
         })
-    ]
+    ],
+    secret: process.env.SECRET,
+    session: {
+      strategy: "jwt",
+    
+      maxAge: 30 * 24 * 60 * 60, // 30 days
+    
+      updateAge: 24 * 60 * 60, // 24 hours
+    },
+    jwt: {
+      secret: process.env.SECRET,
+      maxAge: 60 * 60 * 24 * 30,
+      
+      async encode({ secret, token, maxAge }) {},
+      async decode({ secret, token }) {},
+    }  
 })
