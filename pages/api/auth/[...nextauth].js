951e1ab98a6d0ba85c396cb39c21f2ead57cd160
Glenn Pickett <donnegas@gmail.com>
2022-01-12 19:15:39 -0500

    Next 4

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth';
+import { getToken } from "next-auth/jwt"
 import FacebookProvider from 'next-auth/providers/facebook'
 
 export default NextAuth({
@@ -16,4 +17,16 @@ export default NextAuth({
       maxAge: 30 * 24 * 60 * 60, // 30 days
       updateAge: 24 * 60 * 60, // 24 hours
     },
+    jwt: {
+      // A secret to use for key generation. Defaults to the top-level `secret`.
+      secret: process.env.SECRET,
+      // The maximum age of the NextAuth.js issued JWT in seconds.
+      // Defaults to `session.maxAge`.
+      maxAge: 60 * 60 * 24 * 30,
+      // You can define your own encode/decode functions for signing and encryption
+      // if you want to override the default behavior.
+      async encode({ secret, token, maxAge }) {},
+      async decode({ secret, token }) {},
+    },
+    debug: true,
 })
