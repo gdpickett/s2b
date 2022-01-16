6f7096e926d442845d2760b71f173590ad49f0a1
Glenn Pickett <donnegas@gmail.com>
2022-01-16 13:03:24 -0500

    Iron 3a

--- Components/FacebookLogin.js
+++ Components/FacebookLogin.js
@@ -125,9 +125,9 @@ function FacebookLoginComponent({ session }, props) {
             {login && (
                 <div className="card">
                     <div className="card-body">
-                        <Image className="rounded" src={picture} alt="Profile" />
+                        {/*<Image className="rounded" src={picture} alt="Profile" />
                         <h5 className="card-title">{data.name}</h5>
-                        <p className="card-text">Email ID: {data.email}</p>
+                        <p className="card-text">Email ID: {data.email}</p>*/}
                         <a href="#" className="btn btn-danger btn-sm" onClick={logout}>
                             Logout
                         </a>
