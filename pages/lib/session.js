8b7b433447b38d23ee1a85475aed4fd1894cfeb4
Glenn Pickett <donnegas@gmail.com>
2022-01-16 07:50:22 -0500

    Next 5

--- pages/lib/session.js
+++ pages/lib/session.js
@@ -1,7 +1,9 @@
-export default sessionOptions = {
-    password: process.env.SECRET,
-    cookieName: "salon2bomb",
-    cookieOptions: {
-      secure: process.env.NODE_ENV === "production"|false,
-    },
-  };
+export default sessionOptions(
+    {
+        password: process.env.SECRET,
+        cookieName: "salon2bomb",
+        cookieOptions: {
+            secure: process.env.NODE_ENV === "production" | false,
+        }
+    }
+)
